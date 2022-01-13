84312bff92081b612a1e87954350b1708e67206a
Joris Masson <joris.masson2002@gmail.com>
2022-01-13 15:21:38 +0100

    reformaté le player.py

--- tests/ecos/player.py
+++ tests/ecos/player.py
@@ -1,32 +1,34 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
     # Element graphique du jeu qui peut interargir avec d'autres sprite
 
     def __init__(self, x, y):
-        super().__init__() # Initialisation du sprite
-        self.sprite_sheet = pygame.image.load('player.png') # Chargement de la map
-        self.image = self.get_image(0, 0)  # Récupère l'image 0,0 de la decoupe en 32px, pour avoir l'image 2 de la ligne 1 on va faire 32,0 etc
-        self.image.set_colorkey([0, 0, 0]) # Couleur de fond en noir
+        super().__init__()  # Initialisation du sprite
+        self.sprite_sheet = pygame.image.load('player.png')  # Chargement de la map
+        self.image = self.get_image(0,
+                                    0)  # Récupère l'image 0,0 de la decoupe en 32px, pour avoir l'image 2 de la ligne 1 on va faire 32,0 etc
+        self.image.set_colorkey([0, 0, 0])  # Couleur de fond en noir
         self.rect = self.image.get_rect()
-        self.position = [x, y] # Récupère la position du joueur
+        self.position = [x, y]  # Récupère la position du joueur
         self.images = {
             'down': self.get_image(0, 0),
             'right': self.get_image(0, 64),
             'left': self.get_image(0, 32),
             'up': self.get_image(0, 96)
         }
-        self.feet = pygame.Rect(0, 0, self.rect.width*0.5, 12) # Pied du joueur de la taille de la moitié du joueur
-        self.oldposition = self.position.copy() # Copie de la position du joueur avant qu'il ne bouge
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)  # Pied du joueur de la taille de la moitié du joueur
+        self.oldposition = self.position.copy()  # Copie de la position du joueur avant qu'il ne bouge
 
     def save_location(self):
         self.oldposition = self.position.copy()
 
-    def animation(self, name): # Va chercher l'image à appliquer à l'appui dela touche
+    def animation(self, name):  # Va chercher l'image à appliquer à l'appui dela touche
         self.image = self.images[name]
-        self.image.set_colorkey([0, 0, 0]) # Couleur de fond en noir
+        self.image.set_colorkey([0, 0, 0])  # Couleur de fond en noir
 
-    def move_right(self): # Se déplace de vitesse 2
+    def move_right(self):  # Se déplace de vitesse 2
         self.position[0] += 2
 
     def move_left(self):
@@ -38,16 +40,16 @@ class Player(pygame.sprite.Sprite):
     def move_down(self):
         self.position[1] += 2
 
-    def update(self): # Récupère la position de base
+    def update(self):  # Récupère la position de base
         self.rect.topleft = self.position
-        self.feet.midbottom = self.rect.midbottom # Positionner les pieds par rapport au rectangle
+        self.feet.midbottom = self.rect.midbottom  # Positionner les pieds par rapport au rectangle
 
     def move_collision(self):
         self.position = self.oldposition
         self.rect.topleft = self.position
         self.feet.midbottom = self.rect.midbottom  # Positionner les pieds par rapport au rectangle
 
-    def get_image(self, x, y): # Fonction pour retourner la map avec les sprites
+    def get_image(self, x, y):  # Fonction pour retourner la map avec les sprites
         image = pygame.Surface([32, 32])
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
