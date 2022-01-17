53e4ff835308e0a51be138d589059fe69d622c1a
JojoAz1605 <joris.masson2002@gmail.com>
2022-01-17 15:07:35 +0100

    détruks

--- tests/ecos/player.py
+++ tests/ecos/player.py
@@ -5,8 +5,15 @@ from brain import Brain
 class Player(pygame.sprite.Sprite):
     # Element graphique du jeu qui peut interargir avec d'autres sprite
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, gender, name, health, attack, age, lifetime):
         super().__init__()  # Initialisation du sprite
+        self.name = name  # Variable nom
+        self.gender = gender  # Variable sexe
+        self.age = age  # Variable age
+        self.health = health  # Variable vie
+        self.attack = attack  # Variable attaque
+        self.lifetime = lifetime  # Variable durée de vie
+        self.weapon = None  # Variable arme initialisée à None car il n'a pas d'arme en main au début du jeu
         self.sprite_sheet = pygame.image.load('player.png')  # Chargement de la map
         self.image = self.get_image(0, 0)
         # Récupère l'image 0,0 de la decoupe en 32 px, pour avoir l'image 2 de la ligne 1 on va faire 32,0 etc
@@ -60,3 +67,31 @@ class Player(pygame.sprite.Sprite):
         image = pygame.Surface([32, 32])  # Le perso fait 32x32
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
+
+    def get_all(self):
+        return tuple((self.name, self.gender, self.health, self.attack, self.age, self.lifetime))
+
+    def set_all(self, name, gender, health, attack, age, lifetime):
+        self.age = age
+        self.gender = gender
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.lifetime = lifetime
+
+    def damage(self, damage):
+        self.health -= damage
+        print(f"Aie, {self.name} vient de subir {damage} dégâts et possède maintenant {self.health} points de vie")
+
+    def player_attack(self, target_player):
+        damage = self.attack
+        if self.has_weapon():
+            damage += self.weapon.damage
+        target_player.damage(damage)
+
+    def set_weapon(self, weapon):
+        self.weapon = weapon
+        print(f"{self.name} a trouvé {self.weapon.name} et gagne {self.weapon.damage} points d'attaque")
+
+    def has_weapon(self):
+        return self.weapon is not None
